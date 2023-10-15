dd57db7ad959c5e08f6d7eda8d8cb7a3572bca17
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-10-15 17:38:44 +0300

    fix: navigate to movies page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
   
   useEffect(() => {
     if (isLogged) {
-      navigate('/');
+      navigate('/movies');
     }
   }, [isLogged, navigate]);
 
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -31,7 +31,7 @@ const Register = () => {
   
   useEffect(() => {
     if (isLogged) {
-      navigate('/');
+      navigate('/movies');
     }
   }, [isLogged, navigate]);
 
