372eff8551526cb5cf519689a609d9ca325005a7
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-10-31 17:52:25 +0300

    feat: add small preloader

--- src/components/PreLoader/PreLoader.jsx
+++ src/components/PreLoader/PreLoader.jsx
@@ -1,9 +1,21 @@
+import { PropTypes } from 'prop-types';
 import './PreLoader.css';
 
-const PreLoader = () => {
+const PreLoader = ({ isSmall, color }) => {
   return (
-    <span className="loader__circle"></span>
+    <span 
+      className={`loader__circle ${isSmall ? 'loader__circle_small' : ''}`}
+      style={{
+        borderColor: color || 'white',
+        borderBottomColor: 'transparent',
+      }}
+    ></span>
   );
+};
+
+PreLoader.propTypes = {
+  isSmall: PropTypes.bool,
+  color: PropTypes.string,
 };
 
 export default PreLoader;
